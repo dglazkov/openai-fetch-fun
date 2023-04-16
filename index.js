5e08f8a77383f1c6c4715acf2ed3e1c1e02bd92d
Dimitri Glazkov <dimitri@glazkov.com>
2023-04-16 11:34:23 -0700

    Working stream!

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import { config } from "dotenv";
 config();
 
 const client = new OpenAIClient(process.env.OPENAI_API_KEY);
-const response = await client.createCompletion({
+const stream = await client.streamCompletion({
   model: "text-davinci-003",
   prompt: "Give me some lyrics, make it up.",
   max_tokens: 256,
   temperature: 0,
 });
 
-console.log(response.completion);
+for await (const chunk of stream) {
+  process.stdout.write(chunk.completion);
+}

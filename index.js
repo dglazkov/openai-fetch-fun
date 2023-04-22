438496ce5606a16c3a5aeb4a7622837da1c304ad
Dimitri Glazkov <dimitri@glazkov.com>
2023-04-21 21:07:54 -0700

    Working streaming chat Completion.

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ import { config } from "dotenv";
 
 config();
 
-const client = new OpenAIClient(process.env.OPENAI_API_KEY);
-const stream = await client.streamCompletion({
-  model: "text-davinci-003",
-  prompt: "Give me some lyrics, make it up.",
-  max_tokens: 256,
-  temperature: 0,
-});
+{
+  const client = new OpenAIClient(process.env.OPENAI_API_KEY);
+  const stream = await client.streamCompletion({
+    model: "text-davinci-003",
+    prompt: "Give me some lyrics, make it up.",
+    max_tokens: 256,
+    temperature: 0,
+  });
 
-for await (const chunk of stream) {
-  process.stdout.write(chunk.completion);
+  for await (const chunk of stream) {
+    process.stdout.write(chunk.completion);
+  }
+  process.stdout.write("\n\n\n");
 }
-process.stdout.write("\n");
+
+{
+  const client = new OpenAIClient(process.env.OPENAI_API_KEY);
+  const stream = await client.streamChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello!" }],
+  });
+
+  for await (const chunk of stream) {
+    if (!chunk.message.content) continue;
+    process.stdout.write(chunk.message.content);
+  }
+  process.stdout.write("\n\n\n");
+}
